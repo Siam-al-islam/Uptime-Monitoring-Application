ce2891af5bf8b0e4f9ba493a22d5d4ef6f454479
Siam-al-islam <siamalislam692@gmail.com>
2025-02-24 21:06:54 +0600

    Add URL parsing and request handling logic in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // app object - module scaffolding
 const app = {};
@@ -25,8 +26,20 @@ app.createServer = () => {
 
 // Handle requests and responses
 app.handleReqRes = (req, res) => {
+    // Request handle
+    // get the url and parse it
+    const parserUrl = url.parse(req.url, true);
+    const path = parserUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parserUrl.query;
+    const headersObject = req.headers;
+
+    console.log(queryStringObject);
+    console.log(headersObject);
+    console.log(trimmedPath);
     // Response handle
-    res.end('Hello Programmer!');
+    res.end('Hello world!');
 };
 
 // Start the server
